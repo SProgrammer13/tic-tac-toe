1015cf21ebd9f4f5d3b867bd68f600004d749370
SProgrammer13 <Aleksandrafomina2022@gmail.com>
2025-11-17 21:19:19 +0200

    theme started

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -6,10 +6,15 @@ function Settings(){
     const navigate = useNavigate();
     const { timeLimit, setTimeLimit, theme, setTheme } = useGame();
     return (
-        
-        <div className="input-time">
-            <label>Game time limit: </label>
-            <input onChange={(e) => setTimeLimit(Number(e.target.value))} type="number" min="10" max='90'/>
+        <div>
+            <div className="input-time">
+                <label>Game time limit: </label>
+                <input onChange={(e) => setTimeLimit(Number(e.target.value))} type="number" min="10" max='90'/>
+            </div>
+            <div className="theme-btn">
+                <div onClick={checkTheme} className="dark-theme">Dark</div>
+                <div onClick={checkTheme} className="light-theme">Light</div>
+            </div>
         </div>
     )
 }
